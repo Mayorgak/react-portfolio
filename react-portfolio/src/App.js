b3f2daa68cedd8fb89541fa6f4633fed9eebe3a0
Kristen M <kristenmayorga@gmail.com>
2020-10-16 23:28:28 -0400

    navbar completed with toggle

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Nav from "react-bootstrap/Nav";
 import './App.css';
 
 class App extends React.Component {
@@ -35,8 +35,22 @@ class App extends React.Component {
     <div>
       <Router>
         <Container className="p-0" fluid={true}>
-          <Navbar className="border-bottom">
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Kristen Mayorga</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </Container>
       </Router>
