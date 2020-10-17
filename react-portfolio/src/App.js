65543f92f05916437c3179f249b15a3d63fbddd1
Kristen M <kristenmayorga@gmail.com>
2020-10-17 00:13:25 -0400

    added home route

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -6,6 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import './App.css';
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -34,7 +37,7 @@ class App extends React.Component {
 
   render(){
   return (
-    <div>
+    
       <Router>
         <Container className="p-0" fluid={true}>
           <Navbar className="border-bottom" bg="transparent" expand="lg">
@@ -51,18 +54,23 @@ class App extends React.Component {
                 <Link className="nav-link" to="/contact">
                   Contact
                 </Link>
+
               </Nav>
             </Navbar.Collapse>
           </Navbar>
 
-        <Footer>
-          {}
-        </Footer>
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} /> 
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+           
+        <Footer/>
+          
+        
 
 
         </Container>
       </Router>
-    </div>
+    
   );
 }
 }
---
+++ react-portfolio/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function HomePage() {
+    return (
+        <>
+        <p>HomePage Works!</p>
+        </>
+    );
+}
+
+export default HomePage;
