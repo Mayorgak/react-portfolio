0f0fd813311608490c3189f57b140e5626ebaa19
Kristen M <kristenmayorga@gmail.com>
2020-10-18 20:58:00 -0400

    deploy

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -2,40 +2,29 @@ import React from 'react';
 import CardInfo from '../components/CardInfo';
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Container } from 'react-bootstrap';
+
 
 
 
 function card(props) {
   console.log(props);
     return (
-      <Card style={{ width: "20rem" }} className="i" >
-        <Card.Img variant="top" src={props.item.imgSrc} />
-        <Card.Body>
-          <Card.Title classsName="text-center">{props.item.title}</Card.Title>
-          <Card.Text>{props.item.subTitle}</Card.Text>
-          <Button variant="primary" target="_blank" href={props.item.link}>
-            Check it out
-          </Button>
-        </Card.Body>
-      </Card>
+      <div className="card-style">
+        <Card style={{ width: "20rem" }} className="">
+          <Card.Img variant="top" src={props.item.imgSrc} />
+          <Card.Body>
+            <Card.Title classsName="text-center">{props.item.title}</Card.Title>
+            <Card.Text>{props.item.subTitle}</Card.Text>
+            <Button variant="primary" target="_blank" href={props.item.link}>
+              Check it out
+            </Button>
+          </Card.Body>
+        </Card>
 
-      // <div
-      //   className="d-inline-block k-card"
-      //   onClick={(e) => props.click(props.item)}
-      // >
-      //   <img
-      //     className="k-card-image"
-      //     src={props.item.imgSrc}
-      //     alt={props.item.imgSrc}
-      //   />
-      //   {props.item.selected && (
-      //     <CardInfo
-      //       title={props.item.title}
-      //       subTitle={props.item.subTitle}
-      //       link={props.item.link}
-      //     />
-      //   )}
-      // </div>
+      </div>
     );
 
     
