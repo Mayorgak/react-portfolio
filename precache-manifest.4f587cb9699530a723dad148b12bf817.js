ff8dc97cb5c0c833a298e40f1455feeb4cf70743
Kristen M <kristenmayorga@gmail.com>
2020-10-18 20:58:54 -0400

    Updates

---
+++ precache-manifest.4f587cb9699530a723dad148b12bf817.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13adfb5d502e4eb765be396c52a9e1b5",
+    "url": "/photo-port/index.html"
+  },
+  {
+    "revision": "0340292963b83b427719",
+    "url": "/photo-port/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1914d2fc503385e32402",
+    "url": "/photo-port/static/css/main.a8d28295.chunk.css"
+  },
+  {
+    "revision": "0340292963b83b427719",
+    "url": "/photo-port/static/js/2.d5295d41.chunk.js"
+  },
+  {
+    "revision": "3dee93c0ac0c6b2a029dc99b08a2765f",
+    "url": "/photo-port/static/js/2.d5295d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1914d2fc503385e32402",
+    "url": "/photo-port/static/js/main.2122684c.chunk.js"
+  },
+  {
+    "revision": "aa0e969ea7cf66d801c8",
+    "url": "/photo-port/static/js/runtime-main.75070f25.js"
+  },
+  {
+    "revision": "2fc842e9799041f36134295acb8a1323",
+    "url": "/photo-port/static/media/Kristen.2fc842e9.png"
+  },
+  {
+    "revision": "53926c6d3991615878506a08b265e401",
+    "url": "/photo-port/static/media/To-do-list.53926c6d.png"
+  },
+  {
+    "revision": "180f5d6995349db5c199509ea65b8d85",
+    "url": "/photo-port/static/media/drinkology.180f5d69.png"
+  },
+  {
+    "revision": "c67bfba8855a02f1d5ff93332219ba7b",
+    "url": "/photo-port/static/media/resume.c67bfba8.png"
+  },
+  {
+    "revision": "cc9f94384387467ba66fae1fb49bf27b",
+    "url": "/photo-port/static/media/workday.cc9f9438.png"
+  },
+  {
+    "revision": "10f0ef19cd4a2c157181601d61a4cb90",
+    "url": "/photo-port/static/media/yellow.10f0ef19.jpg"
+  }
+]);
